1f0233a3b96d777468b59ef4cd76c48799836561
tmaPeniel <taouemap@gmail.com>
2025-06-20 13:49:22 +0200

    remove Reading plan diagnostic file

--- src/components/ReadingPlan.tsx
+++ src/components/ReadingPlan.tsx
@@ -6,7 +6,6 @@
 
 import React, { useState } from 'react';
 import OptimizedReadingPlan from './OptimizedReadingPlan';
-import ReadingPlanDiagnostics from './ReadingPlanDiagnostics';
 
 interface ReadingPlanProps {
   dayNumber: number;
